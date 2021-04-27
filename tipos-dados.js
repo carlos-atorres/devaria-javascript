7064c6c33988983c922ae6668edaf5dc1db3a429
carlos-atorres <carlosalbertorres318@gmail.com>
2021-04-27 20:52:49 -0300

    Primeiro commit de JavaScript

---
+++ tipos-dados.js
@@ -0,0 +1,20 @@
+const inteiro = 10;
+const decimal = 10.5;
+const bool = false;
+const nome = "Douglas";
+let copo = null;
+let televisao;
+let pessoa = {
+    nome: "Carlos"
+}
+
+const symb = Symbol();
+
+console.log(typeof inteiro);
+console.log(typeof decimal);
+console.log(typeof bool);
+console.log(typeof nome);
+console.log(typeof copo);
+console.log(typeof televisao);
+console.log(typeof pessoa);
+console.log(typeof symb);
---
+++ variaveis-constantes.js
@@ -0,0 +1,5 @@
+var cor = "branca";
+let nome = "Carlos"; //var é global, let é uma variável de escopo local
+const empresa = "FusionTech";
+
+console.log(cor, nome, empresa);
