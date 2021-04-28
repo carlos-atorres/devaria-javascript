36ed38592e09f86c4c03b51c5ce7620bd451dbee
carlos-atorres <carlosalbertorres318@gmail.com>
2021-04-27 21:49:23 -0300

    Aula JavaScript - Parte 3

---
+++ estrutura-selecao.js
@@ -0,0 +1,11 @@
+const valido = false;
+let nome = "Carlos";
+let idade = 25;
+
+
+if (valido || idade >= 50) {
+    console.log("expressão válida - idade maior que 24");
+} else {
+    console.log("else");
+}
+
